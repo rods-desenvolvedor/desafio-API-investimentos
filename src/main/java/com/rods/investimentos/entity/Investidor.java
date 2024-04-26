286a279d5b642a5f938c894bdb95b6b513141ae6
rods <rods.desenvolvedor@gmail.com>
2024-04-26 17:18:00 -0400

    estrutura inicial das entidades

--- src/main/java/com/rods/investimentos/entity/Investidor.java
+++ src/main/java/com/rods/investimentos/entity/Investidor.java
@@ -1,5 +1,53 @@
 package com.rods.investimentos.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "investidores")
 public class Investidor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @OneToMany
+    @JoinColumn(name = "id_investimentos")
+    private List<Investimento> investimentos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Investimento> getInvestimentos() {
+        return investimentos;
+    }
+
+    public void setInvestimentos(List<Investimento> investimentos) {
+        this.investimentos = investimentos;
+    }
+
+    
+
     
 }
--- src/main/java/com/rods/investimentos/entity/Investimento.java
+++ src/main/java/com/rods/investimentos/entity/Investimento.java
@@ -1,5 +1,66 @@
 package com.rods.investimentos.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "investimentos")
 public class Investimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate dataInvestimento;
+    private BigDecimal valorInvestimento;
+    
+    @ManyToOne
+    //@JoinColumn(name = "id_investidor")
+    @JsonIgnore
+    private Investidor investidor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataInvestimento() {
+        return dataInvestimento;
+    }
+
+    public void setDataInvestimento(LocalDate dataInvestimento) {
+        this.dataInvestimento = dataInvestimento;
+    }
+
+    public BigDecimal getValorInvestimento() {
+        return valorInvestimento;
+    }
+
+    public void setValorInvestimento(BigDecimal valorInvestimento) {
+        this.valorInvestimento = valorInvestimento;
+    }
+
+    public Investidor getInvestidor() {
+        return investidor;
+    }
+
+    public void setInvestidor(Investidor investidor) {
+        this.investidor = investidor;
+    }
+
+    
+
     
 }
