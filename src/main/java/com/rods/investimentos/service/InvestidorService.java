234626c5f94bf61cdc3702473821e9c06a3b67e7
rods <rods.desenvolvedor@gmail.com>
2024-04-28 10:44:00 -0400

    criação do servico do investidor

---
+++ src/main/java/com/rods/investimentos/service/InvestidorService.java
@@ -0,0 +1,28 @@
+package com.rods.investimentos.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rods.investimentos.entity.Investidor;
+import com.rods.investimentos.entity.Investimento;
+import com.rods.investimentos.repository.InvestidorRepository;
+
+@Service
+public class InvestidorService {
+
+    private InvestidorRepository investidorRepository;
+
+    public InvestidorService(InvestidorRepository investidorRepository){
+        this.investidorRepository = investidorRepository;
+    }
+
+    public Investidor registrarInvestidor(Investidor investidor){
+        return investidorRepository.save(investidor);
+    }
+
+    public List<Investidor> listarInvestidores(){
+        return investidorRepository.findAll();
+    }
+    
+}
